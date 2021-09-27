8715ae030f14bfabf8d6385ace2eea37c58cbdfc
Bilal <bilal1909@gmail.com>
2021-09-28 04:58:53 +0500

    Add

--- src/component/About.js
+++ src/component/About.js
@@ -42,7 +42,6 @@ export default function About() {
                     </li>
                   </ul>
                   <a href={pdfile} download>
-                    
                     Download CV
                   </a>
                 </div>
